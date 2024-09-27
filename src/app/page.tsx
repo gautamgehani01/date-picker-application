b8b116eccbec4196d78c5e6372ec32a41817053b
gautamgehani01 <104668401+gautamgehani01@users.noreply.github.com>
2024-09-28 02:42:51 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import DateTimePickerForm from '@/components/Time-picker/date-time-picker-form';
+
+export default function Home() {
+  return (
+    <div className='my-10 max-w-md mx-auto'>
+      <DateTimePickerForm />
+    </div>
+  );
+}
